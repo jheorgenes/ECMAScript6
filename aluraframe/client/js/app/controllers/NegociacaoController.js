ac5e54bb24a68ac124db326f37e792d94ba0b177
Jheorgenes Warlley <jheorgenes@gmail.com>
2021-08-10 23:38:47 -0300

    first commit

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,17 @@
+class NegociacaoController {
+
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        let data = new Date(this._inputData.value.split('-'))
+
+        console.log(data);
+    }
+}
